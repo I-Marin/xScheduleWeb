7ae9bcc010cc8df3f0f81438e937c69ad42b3785
Ariel <76741349+I-Marin@users.noreply.github.com>
2023-12-11 19:53:20 +0100

    baseUrl especifico para el front

---
+++ assets/js/baseUrl.js
@@ -0,0 +1,12 @@
+const ENVS = { LOCAL: '127.0.0.1', PRO: '192.168.1.99' }
+const DEFAULT_PORT = '31500'
+
+const ACTUAL_ENV = ENVS.PRO
+
+const BASE_URL = (port) => {
+    if (ACTUAL_ENV !== ENVS.LOCAL && !port) {
+        port = DEFAULT_PORT
+    }
+
+    return !port ? ACTUAL_ENV : `${ACTUAL_ENV}:${port}`
+}
